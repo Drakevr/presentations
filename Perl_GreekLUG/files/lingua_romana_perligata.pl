aae7f1a8d4e6eeffc0d54d63a72710a215d0f67d
Alex-P. Natsios <drakevr@2f30.org>
2015-06-07 11:10:39 +0300

    Slides for the perl intro done in greeklug (2015)
    A simple intro to perl{5,6} for the local LUG in Pylaia, Thessaloniki.gr

---
+++ Perl_GreekLUG/files/lingua_romana_perligata.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl 
+
+use Lingua::Romana::Perligata;
+
+maximum inquementum tum biguttam egresso scribe.
+meo maximo vestibulo perlegamentum da.
+da duo tum maximum conscribementa meis listis.
+
+dum listis decapitamentum damentum nexto
+    fac sic
+       nextum tum novumversum scribe egresso.
+       lista sic hoc recidementum nextum cis vannementa da listis.
+    cis.
---
+++ Perl_GreekLUG/files/obfuscated.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+$|--;for($/=1/10;$/<=1/2;$/+=50e-7){$\=qq/J,\r/;
+substr$\,$/*length$\,$/-$/,$_,for split/(.{2})/,
+q/ursetk caanho tlhreerP /;print'';}warn qq/\n/;
+
+#  by choroba from PerlMonks.org
